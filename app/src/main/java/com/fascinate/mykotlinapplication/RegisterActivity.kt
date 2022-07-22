53b7dd6dd109b0d002e3dc8e96bcbc35f637c3ab
Usman <shahjaayy@gmail.com>
2022-07-22 08:51:54 +0500

    Working on RegisterActivity.kt

--- app/src/main/java/com/fascinate/mykotlinapplication/RegisterActivity.kt
+++ app/src/main/java/com/fascinate/mykotlinapplication/RegisterActivity.kt
@@ -2,10 +2,33 @@ package com.fascinate.mykotlinapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.fascinate.mykotlinapplication.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.tvLoginAsGuest.setOnClickListener {
+            guestLogin()
+        }
+        binding.tvLoginAsGmail.setOnClickListener{
+            gmailLogin()
+        }
+
+    }
+
+    private fun gmailLogin() {
+
+    }
+
+    private fun guestLogin() {
+
     }
 }
