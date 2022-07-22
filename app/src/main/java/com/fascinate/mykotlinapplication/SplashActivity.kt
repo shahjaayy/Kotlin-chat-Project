f24fca1942e8969d5a4ae4c520a0f774f588a22d
Usman <shahjaayy@gmail.com>
2022-07-22 08:32:55 +0500

    Updated Splash Code

--- app/src/main/java/com/fascinate/mykotlinapplication/SplashActivity.kt
+++ app/src/main/java/com/fascinate/mykotlinapplication/SplashActivity.kt
@@ -1,11 +1,31 @@
 package com.fascinate.mykotlinapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import java.util.*
+import kotlin.concurrent.schedule
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Timer().schedule(4000){
+            myStartActivity()
+        }
+
+    }
+
+    private fun myStartActivity() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
